7d831ce39dcd303f1b3befa6c7d746912b612583
SimoneMichelon <simomike2015@gmail.com>
2024-06-09 00:59:12 +0200

    initializing repo

---
+++ src/main/java/osiride/vitt/VittApplication.java
@@ -0,0 +1,12 @@
+package osiride.vitt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VittApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VittApplication.class, args);
+	}
+}
---
+++ src/test/java/osiride/vitt/VittApplicationTests.java
@@ -0,0 +1,13 @@
+package osiride.vitt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VittApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
