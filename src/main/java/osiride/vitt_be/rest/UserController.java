fd32eb6c6257b003cb4ee7082cbdbaead341feb1
SimoneMichelon <simomike2015@gmail.com>
2024-06-13 23:29:27 +0200

    aggiunta di controller e test

---
+++ src/main/java/osiride/vitt_be/rest/UserController.java
@@ -0,0 +1,28 @@
+package osiride.vitt_be.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import osiride.vitt_be.domain.User;
+import osiride.vitt_be.service.UserService;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getAllUsers(){
+		List<User> users = userService.getAllUsers();
+		return ResponseEntity.status(HttpStatus.OK).body(users);
+	}
+	
+}
