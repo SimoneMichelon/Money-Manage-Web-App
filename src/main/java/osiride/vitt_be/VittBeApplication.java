0a7f234ad10750365fbb12149741fb5d38b96276
SimoneMichelon <simomike2015@gmail.com>
2024-06-27 19:11:08 +0200

    enabled logger by lombok

--- src/main/java/osiride/vitt_be/VittBeApplication.java
+++ src/main/java/osiride/vitt_be/VittBeApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VittBeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VittBeApplication.class, args);
 	}
