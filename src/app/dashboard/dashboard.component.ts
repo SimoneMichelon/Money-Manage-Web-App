5bad2f0464af4d728e0ff6d4fab72e3a8c26abaa
SimoneMichelon <simomike2015@gmail.com>
2024-07-03 19:10:48 +0200

    prova  api

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-
+import { UserControllerService } from '../api/services/user-controller.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [MatButtonModule,MatCardModule],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
-} 
+  constructor(private userService: UserControllerService) { }
+
+  ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(users => {
+      console.log(users);
+    });
+  }
+}
