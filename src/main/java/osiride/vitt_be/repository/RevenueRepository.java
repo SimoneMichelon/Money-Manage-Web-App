df24b1603054c2048b4e9db3c468866be5daf6d4
SimoneMichelon <simomike2015@gmail.com>
2024-07-07 05:10:34 +0200

    autorizzazione anche da swagger attivata

--- src/main/java/osiride/vitt_be/repository/RevenueRepository.java
+++ src/main/java/osiride/vitt_be/repository/RevenueRepository.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import osiride.vitt_be.domain.Expense;
 import osiride.vitt_be.domain.Revenue;
 
 @Repository
