750912f22cf9e2e7ea6fe75dbfece5b12dd8c90a
SimoneMichelon <simomike2015@gmail.com>
2024-07-09 00:10:38 +0200

    documentazione vault

--- src/main/java/osiride/vitt_be/service/VaultService.java
+++ src/main/java/osiride/vitt_be/service/VaultService.java
@@ -44,12 +44,18 @@ public class VaultService {
 	private AuthService authService;
 
 	/**
-	 * Get all vaults in database
-	 * @return List<VaultDTO>
-	 * @throws BadRequestException 
-	 * @throws NotFoundException 
-	 * @throws InvalidTokenException 
+	 * Retrieves all VaultDTO objects from the vault repository.
+	 * 
+	 * This method fetches all vault records from the repository and maps them to {@link VaultDTO} objects.
+	 * Only users with admin privileges are authorized to perform this operation.
+	 * 
+	 * @return A list of VaultDTO objects representing all vaults in the repository.
+	 * @throws InvalidTokenException If the authentication token is invalid.
+	 * @throws NotFoundException If the requested resource is not found.
+	 * @throws BadRequestException If there is a problem with the request.
+	 * @throws NotAuthorizedException If the user is not authorized to perform this operation.
 	 * @throws NotAuthorizedException 
+	 * @throws InvalidTokenException 
 	 */
 	public List<VaultDTO> getAll() throws InvalidTokenException, NotFoundException, BadRequestException, NotAuthorizedException{
 		if(authService.isAdmin()) 
@@ -63,12 +69,19 @@ public class VaultService {
 	}
 
 	/**
-	 * Find a vault by Id
-	 * @param id
-	 * @return VaultDTO
-	 * @throws BadRequestException
-	 * @throws InvalidTokenException 
-	 * @throws NotAuthorizedException 
+	 * Finds a VaultDTO by its ID.
+	 * 
+	 * This method retrieves a Vault entity from the repository based on the provided ID and maps it to a VaultDTO.
+	 * If the ID is null, a {@link BadRequestException} is thrown.
+	 * If no Vault is found with the provided ID, a {@link NotFoundException} is thrown.
+	 * Only admins or owners of the Vault are authorized to perform this operation.
+	 * 
+	 * @param id The ID of the Vault to be retrieved.
+	 * @return The VaultDTO corresponding to the provided ID.
+	 * @throws BadRequestException If the provided ID is null.
+	 * @throws NotFoundException If no Vault is found with the provided ID.
+	 * @throws InvalidTokenException If the authentication token is invalid.
+	 * @throws NotAuthorizedException If the user is not authorized to perform this operation.
 	 */
 	public VaultDTO findById(Long id) throws BadRequestException, NotFoundException, InvalidTokenException, NotAuthorizedException{
 		if(id == null) {
@@ -114,11 +127,11 @@ public class VaultService {
 		}
 
 		Vault vault = vaultMapper.toEntity(vaultDTO);
-		
+
 		UserDTO user = authService.isAdmin() ? 
-                userService.findById(vaultDTO.getUserDTO().getId()) : 
-                authService.getPrincipal();
-		
+				userService.findById(vaultDTO.getUserDTO().getId()) : 
+					authService.getPrincipal();
+
 		vault.setUser(userMapper.toEntity(user));
 
 		vault.setId(null);
@@ -131,22 +144,28 @@ public class VaultService {
 	}
 
 	/**
-	 * Update a Vault by Data given by parameter
-	 * @param vaultDTO
-	 * @return VaultDTO
-	 * @throws BadRequestException
-	 * @throws NotFoundException
-	 * @throws DuplicatedValueException
-	 * @throws InvalidTokenException 
-	 * @throws NotAuthorizedException 
+	 * Updates a Vault entity based on the provided VaultDTO.
+	 * 
+	 * This method validates the provided VaultDTO, checks authorization, and updates the corresponding Vault entity in the repository.
+	 * If the VaultDTO is null, its ID is null, or the data is invalid, a {@link BadRequestException} is thrown.
+	 * If the user is not an admin or the owner of the Vault, a {@link NotAuthorizedException} is thrown.
+	 * If no Vault is found with the provided ID, a {@link NotFoundException} is thrown.
+	 * If the update operation results in a duplicated value, a {@link DuplicatedValueException} is thrown.
+	 * 
+	 * @param vaultDTO The VaultDTO containing the data to update the Vault.
+	 * @return The updated VaultDTO.
+	 * @throws BadRequestException If the provided VaultDTO is null, its ID is null, or the data is invalid.
+	 * @throws NotFoundException If no Vault is found with the provided ID.
+	 * @throws DuplicatedValueException If the update operation results in a duplicated value.
+	 * @throws InvalidTokenException If the authentication token is invalid.
+	 * @throws NotAuthorizedException If the user is not authorized to perform this operation.
 	 */
-
 	public VaultDTO update(VaultDTO vaultDTO) throws BadRequestException, NotFoundException ,DuplicatedValueException , InvalidTokenException, NotAuthorizedException{
 		if(vaultDTO == null || vaultDTO.getId() == null || !isDataValid(vaultDTO)) {
 			log.error("SERVICE - Vault Data given is null - UPDATE");
 			throw new BadRequestException();
 		}
-		
+
 		if(!(authService.isAdmin() || isOwner(authService.getPrincipal(),vaultDTO))) {
 			log.error("SERVICE - Vault Operation not allowed - UPDATE");
 			throw new NotAuthorizedException();
@@ -171,14 +190,21 @@ public class VaultService {
 	}
 
 	/**
-	 * Delete a Vault by id
-	 * @param id
-	 * @return VaultDTO
-	 * @throws BadRequestException
-	 * @throws InternalServerException
-	 * @throws NotFoundException
-	 * @throws InvalidTokenException 
-	 * @throws NotAuthorizedException 
+	 * Deletes a Vault entity based on the provided ID.
+	 * 
+	 * This method checks the validity of the provided ID, verifies authorization, and deletes the corresponding Vault entity from the repository.
+	 * If the ID is null, a {@link BadRequestException} is thrown.
+	 * If no Vault is found with the provided ID, a {@link NotFoundException} is thrown.
+	 * Only admins or owners of the Vault are authorized to perform this operation.
+	 * If the deletion fails due to an unknown error, an {@link InternalServerException} is thrown.
+	 * 
+	 * @param id The ID of the Vault to be deleted.
+	 * @return The VaultDTO of the deleted Vault.
+	 * @throws BadRequestException If the provided ID is null.
+	 * @throws InternalServerException If the Vault could not be deleted due to an unknown error.
+	 * @throws NotFoundException If no Vault is found with the provided ID.
+	 * @throws InvalidTokenException If the authentication token is invalid.
+	 * @throws NotAuthorizedException If the user is not authorized to perform this operation.
 	 */
 	public VaultDTO deleteById(Long id) throws BadRequestException, InternalServerException, NotFoundException, InvalidTokenException, NotAuthorizedException{
 		if(id == null) {
@@ -213,13 +239,19 @@ public class VaultService {
 	}
 
 	/**
-	 * Get all vaults by User Id given by parameter
-	 * @param userId
-	 * @return
-	 * @throws BadRequestException
-	 * @throws NotFoundException
-	 * @throws InvalidTokenException 
-	 * @throws NotAuthorizedException 
+	 * Retrieves all VaultDTO objects for a specific user based on the provided user ID.
+	 * 
+	 * This method fetches all vault records associated with a given user from the repository and maps them to VaultDTO objects.
+	 * If the user ID is null, a {@link BadRequestException} is thrown.
+	 * If the user is not found, a {@link NotFoundException} is thrown.
+	 * Only admins or the user themselves are authorized to perform this operation.
+	 * 
+	 * @param userId The ID of the user whose vaults are to be retrieved.
+	 * @return A list of VaultDTO objects representing all vaults associated with the user.
+	 * @throws BadRequestException If the provided user ID is null.
+	 * @throws NotFoundException If no user is found with the provided ID.
+	 * @throws InvalidTokenException If the authentication token is invalid.
+	 * @throws NotAuthorizedException If the user is not authorized to perform this operation. 
 	 */
 	public List<VaultDTO> getAllVaultByUserId(Long userId) throws BadRequestException, NotFoundException, InvalidTokenException, NotAuthorizedException {
 		if(userId == null) {
@@ -253,7 +285,6 @@ public class VaultService {
 		return vaultList;
 	}
 
-
 	/**
 	 * Updates the capital of a Vault based on the provided Operation.
 	 * 
@@ -269,28 +300,6 @@ public class VaultService {
 	 * @throws BadRequestException if the provided Operation is null
 	 * @throws NotFoundException if the Vault corresponding to the Operation is not found
 	 * @throws DuplicatedValueException if the capital update operation results in a duplicated value
-	 * 
-	 * <p>
-	 * Example usage:
-	 * <pre>
-	 * {@code
-	 * try {
-	 *     boolean updated = vaultService.updateCapital(expense);
-	 *     if (updated) {
-	 *         // handle success
-	 *     } else {
-	 *         // handle failure
-	 *     }
-	 * } catch (BadRequestException e) {
-	 *     // handle bad request
-	 * } catch (NotFoundException e) {
-	 *     // handle not found
-	 * } catch (DuplicatedValueException e) {
-	 *     // handle duplicated value
-	 * }
-	 * }
-	 * </pre>
-	 * </p>
 	 * 
 	 * @author Simone
 	 * @throws NotAuthorizedException 
@@ -320,6 +329,17 @@ public class VaultService {
 		return false;
 	}
 
+	
+	/**
+	 * Retrieves all VaultDTO objects for the currently authenticated principal user.
+	 * 
+	 * This method fetches all vault records associated with the currently authenticated user from the repository and maps them to VaultDTO objects.
+	 * 
+	 * @return A list of VaultDTO objects representing all vaults associated with the authenticated user.
+	 * @throws BadRequestException If there is an issue with the request.
+	 * @throws NotFoundException If the authenticated user is not found.
+	 * @throws InvalidTokenException If the authentication token is invalid.
+	 */
 	private boolean isDataValid(VaultDTO vaultDTO) {
 		return (vaultDTO.getName() == null || 
 				vaultDTO.getUserDTO() == null || 
@@ -328,6 +348,17 @@ public class VaultService {
 						: true;
 	}
 
+	/**
+	 * Checks if the given user is the owner of the given vault.
+	 * 
+	 * This method validates that both the user and the vault are not null, and then checks if the user is the owner of the vault.
+	 * If either the user or the vault is null, a {@link BadRequestException} is thrown.
+	 * 
+	 * @param userDTO The UserDTO object representing the user.
+	 * @param vaultDTO The VaultDTO object representing the vault.
+	 * @return true if the user is the owner of the vault, false otherwise.
+	 * @throws BadRequestException If the user or vault is null.
+	 */
 	private boolean isOwner(UserDTO userDTO , VaultDTO vaultDTO) throws BadRequestException {
 		if(userDTO == null || vaultDTO == null) {
 			throw new BadRequestException();
