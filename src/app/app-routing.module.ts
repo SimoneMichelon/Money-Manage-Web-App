e876c35ce08e84c1b25fed4d41c6e39c48468dc6
Tabarin01 <sandrodesilvestro@gmail.com>
2024-07-18 21:09:57 +0200

    path in routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
-import { MainPageComponent } from './components/main-page/main-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 import { authGuard } from './security/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: AuthComponent},
-  { path: 'dashboard', component: MainPageComponent, canActivate: [authGuard]},
-  
+  { path: 'login', component: AuthComponent },
+  { path: 'dashboard', component: MainPageComponent, canActivate: [authGuard] },
+
   { path: '**', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { 
-
-  
-}
+export class AppRoutingModule {}
